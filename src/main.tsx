da9bc58fe5ca1b22a030f6f28345d82aaf8b7896
Kateryna <tyutyunnikova.katia@gmail.com>
2025-07-05 14:16:08 +0300

    fix folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "modern-normalize/modern-normalize.css";
-import App from "./component/App/App";
+import App from "./components/App/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
